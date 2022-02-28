b88e1d9e57866682810d65659d1382fb02b0c5e8
GitHub Action <action@github.com>
2022-02-28 08:11:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/neday/fluttertoolkit/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/neday/fluttertoolkit/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.neday.fluttertoolkit.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.neday.fluttertoolkit.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/neday/fluttertoolkit/services/MyProjectService.kt
+++ src/main/kotlin/com/github/neday/fluttertoolkit/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.neday.fluttertoolkit.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.neday.fluttertoolkit.MyBundle
 
 class MyProjectService(project: Project) {
 
