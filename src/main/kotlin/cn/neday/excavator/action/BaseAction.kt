4c522efbc13e4331e11a467ff7a6a4f6dbc7ee3a
苏晟 <shengsu15@gmail.com>
2022-03-01 17:37:30 +0800

    use notification

--- src/main/kotlin/cn/neday/excavator/action/BaseAction.kt
+++ src/main/kotlin/cn/neday/excavator/action/BaseAction.kt
@@ -1,15 +1,19 @@
 package cn.neday.excavator.action
 
 import cn.neday.excavator.checker.ProjectChecker
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
-import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.project.Project
 
 abstract class BaseAnAction : AnAction() {
 
+    private var project: Project? = null;
+
     override fun update(event: AnActionEvent) {
-        val project = event.getData(PlatformDataKeys.PROJECT)
+        project = event.getData(PlatformDataKeys.PROJECT)
         val projectPath = project?.basePath
         event.presentation.isEnabledAndVisible = ProjectChecker().check(projectPath)
     }
@@ -19,10 +23,14 @@ abstract class BaseAnAction : AnAction() {
     }
 
     protected fun showInfo(message: String) {
-        Messages.showMessageDialog(message, "Flutter Build Runner Helper", Messages.getInformationIcon())
+        NotificationGroupManager.getInstance().getNotificationGroup("Flutter ToolKit Notification")
+            .createNotification(message, NotificationType.INFORMATION)
+            .notify(project);
     }
 
     protected fun showErrorMessage(message: String) {
-        Messages.showMessageDialog(message, "Flutter Build Runner Helper", Messages.getErrorIcon())
+        NotificationGroupManager.getInstance().getNotificationGroup("Flutter ToolKit Notification")
+            .createNotification(message, NotificationType.ERROR)
+            .notify(project);
     }
 }
